9dee431ca8726f74981b5f30c77e17b4cc7bb107
kikoalfaro <kikoalfaro2@gmail.com>
2018-06-01 00:06:08 +0200

    Actualizada animación del mapa

---
+++ Assets/MapManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void DisableMap()
+    {
+        gameObject.SetActive(false);
+    }
+}
