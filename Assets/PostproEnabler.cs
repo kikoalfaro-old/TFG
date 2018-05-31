3b3c9c47a372ad82c445eb787ff938e1c4c00180
kikoalfaro <kikoalfaro2@gmail.com>
2018-05-31 19:24:15 +0200

    Efecto de postprocesado en cápsula completo

---
+++ Assets/PostproEnabler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostproEnabler : MonoBehaviour {
+
+    public MonoBehaviour[] postProComponents;
+
+    public void EnablePostproComponents()
+    {
+        foreach (MonoBehaviour comp in postProComponents)
+        {
+            comp.enabled = true;
+        }
+    }
+
+    public void DisablePostproComponents()
+    {
+        foreach (MonoBehaviour comp in postProComponents)
+        {
+            comp.enabled = false;
+        }
+    }
+
+}
